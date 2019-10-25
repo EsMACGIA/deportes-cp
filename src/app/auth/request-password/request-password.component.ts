66561642d980ff04031e5169c3246249c097be2d
Manuel Faria <mfaria724@gmail.com>
2019-10-25 03:13:10 -0400

    Request password without backend.

--- src/app/auth/request-password/request-password.component.ts
+++ src/app/auth/request-password/request-password.component.ts
@@ -3,14 +3,15 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { NbLoginComponent, NbAuthService, NB_AUTH_OPTIONS } from '@nebular/auth';
 
 @Component({
   selector: 'nb-request-password-page',
   templateUrl: './request-password.component.html'
 })
-export class NbRequestPasswordComponent {
+export class NbRequestPasswordComponent extends NbLoginComponent {
 
   redirectDelay: number = 0;
   showMessages: any = {};
@@ -21,8 +22,13 @@ export class NbRequestPasswordComponent {
   messages: string[] = [];
   user: any = {};
 
-  constructor(protected cd: ChangeDetectorRef,
-              protected router: Router) {
+  constructor(
+    service: NbAuthService,
+    @Inject(NB_AUTH_OPTIONS) options: {},
+    cd: ChangeDetectorRef,
+    router: Router
+  ) {
+    super(service, options, cd, router);
   }
 
 
