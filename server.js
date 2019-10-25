86a5d4a174e73d530de361179d22203b6d27ae2f
Manuel Faria <mfaria724@gmail.com>
2019-10-25 05:25:28 -0400

    Change app name for deploy.

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/deportes-cp'));
+app.use(express.static(__dirname + '/dist/ngx-admin'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/deportes-cp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ngx-admin/index.html'));
 });
 
 // default Heroku PORT
