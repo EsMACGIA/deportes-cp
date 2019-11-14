1774ca19125520934ffa88b30eb3121f80735b4b
Manuel Faria <mfaria724@gmail.com>
2019-11-14 15:28:11 -0400

    Main layouts.

--- src/app/pages/management/classes/classes.component.ts
+++ src/app/pages/management/classes/classes.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'classes-component',
   templateUrl: './classes.component.html',
   styleUrls: ['./classes.component.scss'],
 })
 export class ClassesComponent {
+
+  constructor(private router: Router) {}
+
+  createClass() {
+    this.router.navigate(['/pages/management/classes-form']);
+  }
 }
   
