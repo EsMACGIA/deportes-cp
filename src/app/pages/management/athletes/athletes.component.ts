a3935bf24fa47fba44a2c286b44510b884c7324b
Manuel Faria <mfaria724@gmail.com>
2019-11-14 11:32:02 -0400

    Athletes main component table.

--- src/app/pages/management/athletes/athletes.component.ts
+++ src/app/pages/management/athletes/athletes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
 
 @Component({
   selector: 'athletes-component',
@@ -6,5 +7,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./athletes.component.scss'],
 })
 export class AthletesComponent {
+
+  settings = {
+    mode: 'external',
+    actions: {
+      columnTitle: 'Acciones'
+    },
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      stock_number: {
+        title: 'N° Acción',
+        type: 'number',
+      },
+      name: {
+        title: 'Nombres',
+        type: 'string',
+      },
+      lastname: {
+        title: 'Apellidos',
+        type: 'string',
+      },
+      ci: {
+        title: 'Cédula de Identidad',
+        type: 'string',
+      },
+      sex: {
+        title: 'Sexo',
+        type: 'string',
+      },
+      birthday: {
+        title: 'Fecha de Nacimiento',
+        type: 'number',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor() {
+    const data = [
+      {
+        id: 1,
+        name: 'Manuel',
+        lastname: 'Faria',
+        sex: 'M',
+        active: true,
+        birthday: '2019-01-01',
+        ci: 'V-25233305', 
+        stock_number: 3258, 
+      },
+      {
+        id: 2,
+        name: 'Juan',
+        lastname: 'Oropeza',
+        sex: 'M',
+        active: true,
+        birthday: '2019-01-01',
+        ci: 'V-26178140', 
+        stock_number: 4523, 
+      }
+    ]
+    this.source.load(data);
+  }
+
+  editAthlete(event) {
+    console.log('Event: ', event)
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('¿Está seguro que desea eliminar este atleta?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 }
   
