db71bbb99f7e4569290d076cad7d292b391d71d7
Manuel Faria <mfaria724@gmail.com>
2019-11-14 10:43:17 -0400

    Main structure for module management,

---
+++ src/app/pages/management/requests-form/requests-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'requests-form-component',
+  templateUrl: './requests-form.component.html',
+  styleUrls: ['./requests-form.component.scss'],
+})
+export class RequestsFormComponent {
+}
+  
