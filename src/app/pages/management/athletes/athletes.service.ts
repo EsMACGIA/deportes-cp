751824ae3e068e25e09a7a405f79c32ef826b478
juan <jaoc1811@gmail.com>
2019-11-15 09:21:23 -0400

    Athletes is now connected to backend

---
+++ src/app/pages/management/athletes/athletes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from '../../../app.base.service';
+import { Observable } from 'rxjs';
+import {AthletesModel} from './athletes.model';
+
+@Injectable()
+export class AthletesService extends BaseService{
+
+    getAthletesList():Observable<any>{
+        return this.getBase('athletes/')
+    }
+
+    createAthletes(athletes:AthletesModel):Observable<any>{
+        return this.postBase(athletes,'athletes/')
+    }
+    deleteAthlete(athlete:AthletesModel):Observable<any>{
+        return this.deleteBase(athlete,'athletes/' + athlete.id + '/')
+    }
+
+    updateAthlete(athlete:AthletesModel):Observable<any>{
+        return this.putBase(athlete,'athletes/')
+    }
+}
