9628f6911ebba05ac5df4ce17a96f56d672e8d59
Manuel Faria <mfaria724@gmail.com>
2019-11-15 08:27:27 -0400

    Main Dash Board.

--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-ecommerce',
+  styleUrls: ['./e-commerce.component.scss'],
   templateUrl: './e-commerce.component.html',
 })
 export class ECommerceComponent {
