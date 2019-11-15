86eaf70be5911ebe56193002e0c7d4d09ff27195
Manuel Faria <mfaria724@gmail.com>
2019-11-15 12:05:04 -0400

    h

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
+//
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
