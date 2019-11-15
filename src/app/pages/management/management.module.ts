14d2c952656583a4f4794747f80497b9deeec6f5
Manuel Faria <mfaria724@gmail.com>
2019-11-15 10:29:39 -0400

    Mwerge.

--- src/app/pages/management/management.module.ts
+++ src/app/pages/management/management.module.ts
@@ -19,21 +19,15 @@ import { ThemeModule } from '../../@theme/theme.module';
 
 import { ManagementRoutingModule, routedComponents } from './management-routing.module';
 
-<<<<<<< HEAD
 import {ClassesService} from './classes/classes.service';
 import {TrainersService} from '../administration/trainers/trainers.service';
 
 //Models
 import {AthletesModel} from './athletes/athletes.model';
 import {ClassesModel} from './classes/classes.model';
-=======
 //Services
 import {AthletesService} from './athletes/athletes.service';
 
-//Models
-import {AthletesModel} from './athletes/athletes.model';
-
->>>>>>> 6c0d46d453785905e32809e49c034275d5efeebb
 
 @NgModule({
   imports: [
@@ -60,13 +54,10 @@ import {AthletesModel} from './athletes/athletes.model';
   ],
   providers: [
     AthletesModel,
-<<<<<<< HEAD
     ClassesService,
     ClassesModel,
     TrainersService,
-=======
     AthletesService,
->>>>>>> 6c0d46d453785905e32809e49c034275d5efeebb
   ],
 })
 export class ManagementModule { }
